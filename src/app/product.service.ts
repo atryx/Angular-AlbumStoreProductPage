bc1c14804faa6d331d7ed279c58e2495844253b4
Mihai Lita <lita.mihai@gmail.com>
2018-02-05 13:45:21 +0200

    Added interfaces, changed return type of getAlbum to Album.

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,5 +1,8 @@
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+
+import { Album } from './album';
 
 import 'rxjs/add/operator/map';
 
@@ -10,9 +13,9 @@ export class ProductService {
 
   constructor(private _http: Http) { }
 
-  getAlbum(id: number) {
+  getAlbum(id: number): Observable<Album> {
     return this._http.get(this._albumUrl).map((response) =>
-      response.json()
+      response.json() as Album
     );
   }
 }
